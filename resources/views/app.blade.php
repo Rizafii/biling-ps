bd41136cbdda3d65b1d30c86bcb0261acc03e5ab
oktavian <oktavianbagasnugroho@gmail.com>
2025-09-25 18:14:04 +0700

    logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,9 +35,9 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="icon" href="/gamepad.svg" sizes="any">
+    <link rel="icon" href="/gamepad.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/gamepad.svg">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
